4359d7d1e52eaaee477512bb9461c0c7c6660c36
Chris Ferone <chrisferone@gmail.com>
2017-11-11 10:43:46 -0500

    Vehicle stays in lane, but movements are jerky

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
+#include "spline.h"
 
 using namespace std;
 
@@ -237,46 +238,59 @@ int main() {
 
           	vector<double> next_x_vals;
           	vector<double> next_y_vals;
-			
+			////////////////////////////////////////////////////////////////////////////////////////////////////
 
           	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
 			
-		  double pos_x;
-          double pos_y;
-          double angle;
-          int path_size = previous_path_x.size();
-
-          for(int i = 0; i < path_size; i++)
-          {
-              next_x_vals.push_back(previous_path_x[i]);
-              next_y_vals.push_back(previous_path_y[i]);
+		  // double pos_x;
+          // double pos_y;
+		  // double pos_s;
+          // double angle;
+          // int path_size = previous_path_x.size();
+			
+		  // //we populate our path list with not-yet visited points from our previous path list	
+          // for(int i = 0; i < path_size; i++)
+          // {
+              // next_x_vals.push_back(previous_path_x[i]);
+              // next_y_vals.push_back(previous_path_y[i]);
+          // }
+		
+			// //If every point in the previous list was executed, then the previous path list will be empty. So we'll set vehicle's position to the current position
+          // if(path_size == 0)
+          // {
+              // pos_x = car_x;
+              // pos_y = car_y;
+              // angle = deg2rad(car_yaw);
+			  // pos_s = car_s;
+          // }
+		  // otherwise we'll say the vehicle position is at the first point in the yet-to-be-executed previous path list
+          // else
+          // {
+              // pos_x = previous_path_x[path_size-1];
+              // pos_y = previous_path_y[path_size-1];
+			  			  
+              // double pos_x2 = previous_path_x[path_size-2];
+              // double pos_y2 = previous_path_y[path_size-2];
+              // angle = atan2(pos_y-pos_y2,pos_x-pos_x2);
+			  // vector<double> sd = getFrenet(pos_x, pos_y, angle, map_waypoints_x, map_waypoints_y);
+			  // pos_s = sd[0];
+          // }
+		  
+		  double target_vel = 49.5;
+          double dist_inc = target_vel * 0.44704 *.02;
+		  vector<double> NewXY;
+          for(int i = 0; i < 50; i++)
+          {   
+			  	
+			  double next_s = car_s + (i + 1) * dist_inc;
+			  double d = 6;
+			  NewXY = getXY(next_s, d, map_waypoints_s, map_waypoints_x, map_waypoints_y);	
+              next_x_vals.push_back(NewXY[0]);
+              next_y_vals.push_back(NewXY[1]);
+              //pos_x += (dist_inc)*cos(angle+(i+1)*(pi()/100));
+              //pos_y += (dist_inc)*sin(angle+(i+1)*(pi()/100));
           }
-
-          if(path_size == 0)
-          {
-              pos_x = car_x;
-              pos_y = car_y;
-              angle = deg2rad(car_yaw);
-          }
-          else
-          {
-              pos_x = previous_path_x[path_size-1];
-              pos_y = previous_path_y[path_size-1];
-
-              double pos_x2 = previous_path_x[path_size-2];
-              double pos_y2 = previous_path_y[path_size-2];
-              angle = atan2(pos_y-pos_y2,pos_x-pos_x2);
-          }
-
-          double dist_inc = 0.5;
-          for(int i = 0; i < 50-path_size; i++)
-          {    
-              next_x_vals.push_back(pos_x+(dist_inc)*cos(angle+(i+1)*(pi()/100)));
-              next_y_vals.push_back(pos_y+(dist_inc)*sin(angle+(i+1)*(pi()/100)));
-              pos_x += (dist_inc)*cos(angle+(i+1)*(pi()/100));
-              pos_y += (dist_inc)*sin(angle+(i+1)*(pi()/100));
-          }
-					
+			//////////////////////////////////////////////////////////////////////////////////////		
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
 
